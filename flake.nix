080ec71aa839ef447ca1c20a3c40a993d4ea9092
Chaz Schlarp <schlarpc@gmail.com>
2026-01-02 08:40:35 +0000

    Pin nixpkgs to 25.11 because node isn't in Hydra cache on unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NFA Form processor with mupdf.js";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
